2fa7b8bf22c05887cfa0dc21f7ddec26b5fc4242
Lee Mason <contact@no-half-pixels.com>
2012-04-14 23:17:21 +0100

    version 1.0.1 stable branch

--- options/fields/upload/field_upload.php
+++ options/fields/upload/field_upload.php
@@ -13,6 +13,7 @@ class NHP_Options_upload extends NHP_Options{
 		parent::__construct();
 		$this->field = $field;
 		$this->value = $value;
+		//$this->render();
 		
 	}//function
 	
@@ -31,9 +32,7 @@ class NHP_Options_upload extends NHP_Options{
 		
 		
 		echo '<input type="hidden" id="'.$this->field['id'].'" name="'.$this->args['opt_name'].'['.$this->field['id'].']" value="'.$this->value.'" class="'.$class.'" />';
-		//if($this->value != ''){
-			echo '<img class="nhp-opts-screenshot" id="nhp-opts-screenshot-'.$this->field['id'].'" src="'.$this->value.'" />';
-		//}
+		echo '<img class="nhp-opts-screenshot" src="'.$this->value.'" />';
 		
 		if($this->value == ''){$remove = ' style="display:none;"';$upload = '';}else{$remove = '';$upload = ' style="display:none;"';}
 		echo ' <a href="javascript:void(0);" class="nhp-opts-upload button-secondary"'.$upload.' rel-id="'.$this->field['id'].'">'.__('Browse', 'nhp-opts').'</a>';
@@ -56,13 +55,11 @@ class NHP_Options_upload extends NHP_Options{
 		
 		wp_enqueue_script(
 			'nhp-opts-field-upload-js', 
-			$this->url.'fields/upload/field_upload.js', 
+			$this->args['theme_url'].'options/fields/upload/field_upload.js', 
 			array('jquery', 'thickbox', 'media-upload'),
 			time(),
 			true
 		);
-		
-		wp_localize_script('nhp-opts-field-upload-js', 'nhp_upload', array('url' => $this->url.'fields/upload/blank.png'));
 		
 	}//function
 	
