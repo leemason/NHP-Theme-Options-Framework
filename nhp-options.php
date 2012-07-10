62a170ceebe292f63a2575f0fa954be445796c67
Sanam Patel <sanam@hencemedia.com>
2012-07-10 21:34:13 +0530

    Update patch-1

--- nhp-options.php
+++ nhp-options.php
@@ -706,7 +706,7 @@ function nhpactive_admin_head() { ?>
 
 <script type="text/javascript">
 jQuery(function(){
-var message = '<p><strong>Theme Activation Successful!</strong> This theme\'s settings are located under <strong><a href="<?php echo admin_url('admin.php?page=nhp_theme_options'); ?>">Theme Options</a></strong>. You can also add some new widget from widget araa <strong><a href="<?php echo admin_url('widgets.php'); ?>">Appearance > Widgets</a></strong>.</p>';
+var message = '<p><strong>Theme Activation Successful!</strong> This theme\'s settings are located under <strong><a href="<?php echo admin_url('admin.php?page=nhp_theme_options'); ?>">Theme Options</a></strong>. You can also add some new widget from widget area <strong><a href="<?php echo admin_url('widgets.php'); ?>">Appearance > Widgets</a></strong>.</p>';
 jQuery('.themes-php #message2').html(message);
 });
 </script>
