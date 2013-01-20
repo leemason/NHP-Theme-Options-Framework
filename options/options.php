d27bb8669f8e897a46e9496e3fc9de7040e4a3af
Lenin <lenin@tasawr.com>
2013-01-20 11:21:43 +0600

    Fixed localized string formatting

--- options/options.php
+++ options/options.php
@@ -60,7 +60,7 @@ class NHP_Options{
 		$defaults['dev_mode'] = true;
 		$defaults['stylesheet_override'] = false;
 		
-		$defaults['footer_credit'] = __('<span id="footer-thankyou">Options Panel created using the <a href="'.$this->framework_url.'" target="_blank">NHP Theme Options Framework</a> Version '.$this->framework_version.'</span>', 'nhp-opts');
+		$defaults['footer_credit'] = sprintf(__('<span id="footer-thankyou">Options Panel created using the <a href="%s" target="_blank">NHP Theme Options Framework</a> Version %s</span>',$this->framework_url,$this->framework_version), 'nhp-opts');
 		
 		$defaults['help_tabs'] = array();
 		$defaults['help_sidebar'] = __('', 'nhp-opts');
